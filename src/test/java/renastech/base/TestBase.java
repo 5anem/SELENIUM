0f852e4d127c3e935c116637f67c906fad74d4fa
svgnhkmglu <sevginhekimoglu@gmail.com>
2021-11-09 20:37:44 -0500

    tabs

---
+++ src/test/java/renastech/base/TestBase.java
@@ -0,0 +1,27 @@
+package renastech.base;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import renastech.utilities.BrowserUtils;
+import renastech.utilities.WebDriverUtil;
+
+import java.util.concurrent.TimeUnit;
+
+public class TestBase {
+
+    protected WebDriver driver;
+
+    @BeforeMethod
+    public void setup(){
+        driver= WebDriverUtil.getDriver("chrome");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+    }
+
+    @AfterMethod
+    public void closing(){
+        BrowserUtils.wait(3);
+        driver.close();
+    }
+}
