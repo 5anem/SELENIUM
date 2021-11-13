2f12edb0ffc77ef369ac67efacf5a1ea38df06e4
svgnhkmglu <sevginhekimoglu@gmail.com>
2021-11-12 20:48:55 -0500

    actions

--- src/test/java/renastech/day9_Actions_POM/c2_ActionsExamples.java
+++ src/test/java/renastech/day9_Actions_POM/c2_ActionsExamples.java
@@ -1,5 +1,6 @@
 package renastech.day9_Actions_POM;
 
+import org.checkerframework.checker.units.qual.A;
 import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
@@ -30,6 +31,65 @@ public class c2_ActionsExamples extends TestBase2 {
         Alert alert=DriverUtil.getDriver().switchTo().alert();
         BrowserUtils.wait(3);
         alert.accept();
+
+    }
+
+    //go to http://demo.guru99.com/test/simple_context_menu.html
+    //right click to right click button
+    //then click on edit button
+    //then accept the allert that pops up
+
+    @Test
+    public void TC2_rightClik(){
+        DriverUtil.getDriver().get("http://demo.guru99.com/test/simple_context_menu.html");
+
+        WebElement rightClickButton=DriverUtil.getDriver().findElement(By.xpath("//span[.='right click me']"));
+
+        actions=new Actions(DriverUtil.getDriver());
+        //right click to right click button
+
+        actions.contextClick(rightClickButton).perform();
+        //then click on edit button
+
+        BrowserUtils.wait(1);
+        WebElement editButton=DriverUtil.getDriver().findElement(By.xpath("//span[.='Edit']"));
+        editButton.click();
+
+        //then accept the allert that pops up
+        BrowserUtils.wait(1);
+        Alert alert=DriverUtil.getDriver().switchTo().alert();
+        alert.accept();
+
+    }
+
+    //go to http://demo.guru99.com/test/drag_drop.html
+    //drag bank word to account section and drop it
+    //then drag 5000 to amount  section and drop it
+
+    @Test
+    public void TC3_dragDropExample(){
+        DriverUtil.getDriver().get("http://demo.guru99.com/test/drag_drop.html");
+
+        //drag bank word to account section and drop it
+            //2 things needs to be done here
+            //first specify location of drag
+            //secon specify location of drop
+        WebElement dragFrom=DriverUtil.getDriver().findElement(By.xpath("//a[.=' BANK ']"));
+        WebElement dropTo=DriverUtil.getDriver().findElement(By.xpath("//ol[@id='bank']/li"));
+
+        actions=new Actions(DriverUtil.getDriver());
+        actions.dragAndDrop(dragFrom,dropTo).perform();
+
+
+        //then drag 5000 to amount  section and drop it
+            //2 things needs to be done here
+            //first specify location of drag
+            //secon specify location of drop
+
+        WebElement dragFrom2=DriverUtil.getDriver().findElement(By.id("fourth"));
+        WebElement dropTo2=DriverUtil.getDriver().findElement(By.xpath("//ol[@id='amt7']//li"));
+
+        actions.dragAndDrop(dragFrom2,dropTo2).perform();
 
     }
 
