6fd02ef7c834185f4c09ddfcb21a261073041004
svgnhkmglu <sevginhekimoglu@gmail.com>
2021-11-16 19:57:01 -0500

    page object model

--- src/test/java/renastech/day_10_POM_ExplicitWait/c1_SmartBearNegativeLogin.java
+++ src/test/java/renastech/day_10_POM_ExplicitWait/c1_SmartBearNegativeLogin.java
@@ -31,5 +31,12 @@ public class c1_SmartBearNegativeLogin extends TestBase2 {
 
     }
 
-
+    //we extend TestBase2 with logic of inheritence
+        //we get before and after method
+    //we create page class
+        //and we stored webelements and some usefull methods
+    //create an object from page to be able to call webelements and methods
+    //you navigate your url
+    //then you pass username ,password and click on login button with one method that we created under SMLogin Page
+    //then we verified text after we try to logged in with a method that again we create under SMLogin Page
 }
